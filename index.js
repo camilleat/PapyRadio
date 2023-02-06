fcb66bbe7efeb9ade08e9fec1e2f2fb09ee354f8
Matys53 <119408070+Matys53@users.noreply.github.com>
2023-02-06 17:11:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-oui
+const fs = require('fs');
+const re = require('socket.io-client');
+
+
+// Read the JSON file
+fs.readFile('./radios.json', 'utf-8', (err, data) => {
+    if (err) throw err;
+
+    // Parse the JSON data
+    const radioData = JSON.parse(data);
+
+    // Get the radio frequency passed as parameter
+    const radioFrequency = process.argv[2];
+
+    // Search for the radio with the given frequency
+    const radio = radioData.radios.find(radio => radio.frequency === radioFrequency);
+    console.log(radio);
+
+    // Check if the radio was found
+    if (radio) {
+        // Get the URL of the radio
+            const radioURL = radio.url;
+            const radioURlString = radioURL.toString();
+            console.log(radioURL);
+            // Play the radio using Volumio
+            const socket = re.io("http://volumio.local");
+            socket.emit('play', {"value": radioURlString});
+            console.log("Playing webRadio : " + radioURlString);
+            //socket.close();
+    } else {
+        console.error(`Radio with frequency ${radioFrequency} not found`);
+    }
+});
+
+
+
