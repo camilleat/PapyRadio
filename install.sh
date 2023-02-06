26942061d60fc89c548f9e9fe3cc22cc1b493ce4
camilleat <118841268+camilleat@users.noreply.github.com>
2023-02-06 14:17:54 +0100

    maj install
    j'ai enlevé plein de trucs inutiles

--- install.sh
+++ install.sh
@@ -1,59 +1,9 @@
 #!/bin/bash
 
 echo "Installing PapyRadio dependencies"
-echo "unload Loopback module if exists"
-sudo rmmod snd_aloop
+sudo apt-get update
+# Install the required packages via apt-get
+sudo apt-get -y install
 
-libpath=/PapyRadio
-derrormess="Failed to extract caps"
-echo "Detecting cpu"
-cpu=$(lscpu | awk 'FNR == 1 {print $2}')
-#echo "$cpu is the cpu"
-
-if [ $cpu = "armv6l" ] || [ $cpu = "armv7l" ] || [ $cpu = "aarch64" ] || [ $cpu = "i686" ];
-then
-	cd $libpath
-        echo "Cpu is $cpu, installing required caps version."
-	sudo cp /PapyRadio/caps-$cpu.tar /caps.tar
-	cd /
-	sudo tar xvf caps.tar
-	sudo rm /caps.tar
-	if [ $? -eq 0 ]
-		then
-			echo "Extracting data"
-		else
-			echo "$derrormess"
-			exit -1
-		fi
-
-else
-
-	echo "Unsupported cpu ($cpu)"
-	exit -1
-fi
-
-if [ ! -f "/PapyRadio/config.json" ];
-	then
-		echo "file doesn't exist, nothing to do"
-	else
-		echo "File exists removing it"
-		sudo rm /PapyRadio/config.json
-fi
-echo "Checking if radios services exist"
-if [ ! -f "/etc/systemd/system/volparametriceq.service" ];
-	then
-		echo "file volparametriceq.service doesn't exist, creating"
-		cp /PapyRadio/radios.tar.gz /
-		cd /
-		sudo tar -xvf radios.tar.gz
-	else
-		echo "volparametriceq.service removing to install new version !"
-		sudo rm /etc/systemd/system/volparametriceq.service
-		cp /PapyRadio/radios.tar.gz /
-		cd /
-		sudo tar -xvf radios.tar.gz
-		rm /radios.tar.gz
-fi
-sudo systemctl daemon-reload
 #required to end the plugin install
 echo "plugininstallend"
